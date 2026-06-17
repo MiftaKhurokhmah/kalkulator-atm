1cfbeb1a0dc0b50c96d9be29b4057ec948e65152
MiftaKhurokhmah <miftakhurokhmah538252@mail.ugm.ac.id>
2026-06-18 00:47:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ else:
     )
 
 # Konversi Kualitatif menjadi Penyesuaian Nominal Rupiah untuk karakteristik non-GIM
-nilai_adj_jenis = 10000000 if jenis_atm == "Setor Tarik (CRM)" else 0
+nilai_adj_jenis = 3000000 if jenis_atm == "Setor Tarik (CRM)" else 0
 nilai_adj_jarak = max(
     0, (100 - jarak_jalan_utama) * 150000
 )  # Semakin dekat jalan utama, penyesuaian semakin tinggi
